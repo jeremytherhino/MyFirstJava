a2a72d1d8db1286e5e22b5ffff6feaf2186670e8
Jeremy Toledano <jer0529465704@gmail.com>
2023-06-14 12:52:23 +0300

    first

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Node<Integer> node = new Node<Integer>(5);
+        node.setNext(new Node<Integer>(1));
+        System.out.println(node);
+    }
+}
